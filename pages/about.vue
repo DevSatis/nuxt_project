552c196d8d834cc2c59bb7071e63a6994446c65c
Satish kumar <satishkumar.kapildev@cambridge.org>
2023-06-14 22:05:16 +0530

    NuxtJS

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequuntur fugiat at. Eum debitis doloremque blanditiis corrupti tempora non unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iure quis consequuntur, explicabo ut, error suscipit, consectetur hic deserunt quam temporibus modi repellendus! Corrupti animi voluptatibus consequuntur laudantium asperiores repudiandae?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+
+</style>
