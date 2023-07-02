0e274af60f45651ddebdc7674f0ac649629b5282
Satish kumar <satishkumar.kapildev@cambridge.org>
2023-07-02 11:23:21 +0530

    Layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">NUXT DOJO</NuxtLink>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
         <h2>Home</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequuntur fugiat at. Eum debitis doloremque blanditiis corrupti tempora non unde.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iure quis consequuntur, explicabo ut, error suscipit, consectetur hic deserunt quam temporibus modi repellendus! Corrupti animi voluptatibus consequuntur laudantium asperiores repudiandae?</p>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,15 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">NUXT DOJO</NuxtLink>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
         <p>Product details for {{ id }}</p>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum doloribus voluptates officiis asperiores accusantium quam.</p>
     </div>
